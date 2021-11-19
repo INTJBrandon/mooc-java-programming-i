c4a76479f6543cbfd12e51875ebcca8a9e0f3ad5
Brandon Tyus <ausis@mail.com>
2021-11-18 23:59:52 -0600

    prints give speed

--- part01-Part01_24.SpeedingTicket/src/main/java/SpeedingTicket.java
+++ part01-Part01_24.SpeedingTicket/src/main/java/SpeedingTicket.java
@@ -7,5 +7,7 @@ public class SpeedingTicket {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here. 
+        System.out.println("Give speed:");
+        
     }
 }
